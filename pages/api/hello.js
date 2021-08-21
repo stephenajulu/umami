fd7a53d4d9d7bf7ddf9437e099731f2b4366ce94
Mike Cao <mike@mikecao.com>
2021-08-20 20:05:08 -0700

    Get root files

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,10 +4,10 @@ import dirTree from 'directory-tree';
 
 export default function handler(req, res) {
   res.status(200).json({
-    name: 'John Doe',
     filename: __filename,
     dirname: __dirname,
     cwd: process.cwd(),
     files: dirTree(process.cwd()),
+    rootFiles: dirTree('/'),
   });
 }
